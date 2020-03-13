18c24913a8e1ac90b5d94726e09a1e6fcc49b64b
Arsen Alkamyan <alkamyan97@gmail.com>
2020-03-14 00:25:23 +0400

    use reducer

--- src/components/contact/SendMessage.tsx
+++ src/components/contact/SendMessage.tsx
@@ -1,12 +1,33 @@
 import * as React from 'react';
 
+interface IState
+{
+    name: string;
+    email: string;
+    message: string;
+}
+
+function reducer(state: IState, { type, value }): IState
+{
+    switch (type)
+    {
+        case "name":
+            return { ...state, name: value };
+            break;
+        case "email":
+            return { ...state, email: value }
+            break;
+        case "message":
+            return { ...state, message: value }
+            break;
+        default:
+            return state;
+    }
+}
+
 function SendMessage()
 {
-    let [state, setState] = React.useState({
-        name: "",
-        email: "",
-        message: "",
-    })
+    let [state, dispatch] = React.useReducer(reducer, { name: "", email: "", message: "" })
     console.log(state)
     return (
         <div>
@@ -26,9 +47,7 @@ function SendMessage()
     function onChange(e: React.SyntheticEvent<HTMLInputElement | HTMLTextAreaElement>)
     {
         const { name, value } = e.currentTarget;
-        let newState = { ...state };
-        newState[name] = value;
-        setState(newState);
+        dispatch({ type: name, value });
     }
 }
 
